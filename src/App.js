b5e9dad2bbfd77e59b9231596f97e49141909eca
Renuka Prasad <renu.yarasu@gmail.com>
2021-09-27 19:07:55 +0530

    01 Birthday Remainder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Data from './Data';
+import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(Data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <section className="container">
+        <h2>{people.length} birthday reminder</h2>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main >
+  )
 
+}
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: 'Person 01',
+        age: 27,
+        image: 'https://images.pexels.com/photos/3586829/pexels-photo-3586829.jpeg',
+    },
+    {
+        id: 2,
+        name: 'Person 02',
+        age: 22,
+        image: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg',
+    },
+    {
+        id: 3,
+        name: 'Person 03',
+        age: 33,
+        image: 'https://images.pexels.com/photos/3585195/pexels-photo-3585195.jpeg',
+    },
+    {
+        id: 4,
+        name: 'Person 04',
+        age: 24,
+        image: 'https://images.pexels.com/photos/2811089/pexels-photo-2811089.jpeg',
+    },
+    {
+        id: 5,
+        name: 'Person 05',
+        age: 25,
+        image: 'https://images.pexels.com/photos/3489796/pexels-photo-3489796.jpeg',
+    },
+];
